679f91099501306791481fe0dda9ec370f2ca2f6
Dongkyu Yu <eastern7star@gmail.com>
2019-03-12 00:08:35 +0900

    Update 2_압축.cpp

--- 3차/2_압축.cpp
+++ 3차/2_압축.cpp
@@ -1,51 +1,34 @@
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
-#include <deque>
 #include <algorithm>
-#include <functional>
-#include <iomanip>
 
 using namespace std;
 
-// 2. æ–√‡
-void compress() {
-	string input = "KAKAO";
-	map<string, int> dict;
-	vector<int> output;
-
-	for (int i = 'A'; i <= 'Z'; i++) {
-		string temp = "";
-		dict.insert(pair<string, int>(temp + char(i), i - 'A' + 1));
-	}
-
-	for (int i = 0; i < input.size(); i++) {
-		string temp = "";
-		temp += input[i];
-		for (int j = i + 1; j < input.size(); j++) {
-			if (dict.find(temp + input[j]) != dict.end()) {
-				temp += input[j];
-				i++;
-			} else {
-				output.push_back(dict[temp]);
-				cout << temp << ", " << dict[temp] << endl;
-				cout << "DICT " << temp + input[j] << endl;
-				dict.insert(pair<string, int>(temp + input[j], dict.size() + 1));
-				temp = "";
-				break;
-			}
-		}
-		if (temp.compare("") != 0)
-			output.push_back(dict[temp]);
-	}
-
-	for (int i = 0; i < output.size(); i++)
-		cout << output[i] << endl;
-}
 
 int main() {
-	compress();
+	int size = 26;
+	map<string, int> dict;
+	for (int i = 0; i < size; i++) {
+		string t = " "; t[0] = 'A' + i;
+		dict.insert({ t, i + 1 });
+	}
+
+	string input;
+	cin >> input;
+
+	vector<int> results;
+	for (int i = 0; i < input.length();) {
+		int sub = 1;
+		while (i + sub <= input.length() && dict.find(input.substr(i, sub)) != dict.end()) sub++;
+		results.push_back(dict[input.substr(i, sub - 1)]);
+		dict.insert({ input.substr(i, sub), size + 1 });
+		size++;
+		i += sub - 1;
+	}
+	for (int r : results)
+		cout << r << endl;
 
 	return 0;
 }
